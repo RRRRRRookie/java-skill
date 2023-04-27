09a2cf3b84d3b63d749b51c3a7df657241f753e3
wanjia1 <capgemini.h.wang@mercedes-benz.com>
2023-04-27 13:54:33 +0800

    🐢 study threadLocal

--- JUC/src/test/java/broken/line/kite/ThreadLocalDemoTest.java
+++ JUC/src/test/java/broken/line/kite/ThreadLocalDemoTest.java
@@ -1,9 +1,13 @@
 package broken.line.kite;
 
+import com.alibaba.ttl.TransmittableThreadLocal;
+import com.alibaba.ttl.threadpool.TtlExecutors;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -23,24 +27,82 @@ public class ThreadLocalDemoTest {
      */
     @Test
     @SneakyThrows
-    void test_jdk_inheritable_thread_local() {
+    void test_jdk_with_out_inheritable_thread_local() {
         final String parentValue = "parent";
         ThreadLocal<String> parentThreadLocal = ThreadLocal.withInitial(() -> parentValue);
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-
-        for (int i = 0; i < 20; i++) {
-
+        int total = 20;
+        CountDownLatch countDownLatch = new CountDownLatch(total);
+        for (int i = 0; i < total; i++) {
+            parentThreadLocal.set(String.format("%s%s", parentValue, i));
             log.info("parent thread {} value is {}", Thread.currentThread().getName(), parentThreadLocal.get());
             executorService.execute(() -> {
                 try {
-                    TimeUnit.MILLISECONDS.sleep(500);
+                    TimeUnit.MILLISECONDS.sleep(50);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                } finally {
+                    countDownLatch.countDown();
                 }
                 log.info("current thread {}  value is {}", Thread.currentThread().getName(), parentThreadLocal.get());
             });
         }
+        countDownLatch.await();
+    }
 
+    /**
+     * 场景一
+     */
+    @Test
+    @SneakyThrows
+    void test_jdk_with_inheritable_thread_local() {
+        final String parentValue = "parent";
+        ThreadLocal<String> parentThreadLocal = InheritableThreadLocal.withInitial(() -> parentValue);
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        int total = 20;
+        CountDownLatch countDownLatch = new CountDownLatch(total);
+        for (int i = 0; i < total; i++) {
+            parentThreadLocal.set(String.format("%s%s", parentValue, i));
+            log.info("parent thread {} value is {}", Thread.currentThread().getName(), parentThreadLocal.get());
+            executorService.execute(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    countDownLatch.countDown();
+                }
+                log.info("current thread {}  value is {}", Thread.currentThread().getName(), parentThreadLocal.get());
+            });
+        }
+        countDownLatch.await();
+    }
+
+    @Test
+    @SneakyThrows
+    void test_jdk_with_ttl_thread_local() {
+        final String parentValue = "parent";
+        ThreadLocal<String> parentThreadLocal = TransmittableThreadLocal.withInitial(() -> parentValue);
+        ExecutorService executorService = TtlExecutors.getTtlExecutorService(Executors.newFixedThreadPool(5));
+        int total = 20;
+        CountDownLatch countDownLatch = new CountDownLatch(total);
+        for (int i = 0; i < total; i++) {
+            String currentValue = String.format("%s%s", parentValue, i);
+            parentThreadLocal.set(currentValue);
+            log.info("parent thread {} value is {}", Thread.currentThread().getName(), parentThreadLocal.get());
+            executorService.execute(() -> {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    countDownLatch.countDown();
+                }
+                log.info("current thread {} parent value is  {}", Thread.currentThread().getName(), parentThreadLocal.get());
+                log.info("current thread {} value is {}, compare value is {}", Thread.currentThread().getName(), parentThreadLocal.get(), Objects.equals(parentThreadLocal.get(), currentValue));
+            });
+        }
+        countDownLatch.await();
     }
 
 
